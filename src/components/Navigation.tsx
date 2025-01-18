c78d73ae7f7bb0bb9a7d37c07cae2bd7ff808291
Gevaun <geniepinguin@gmail.com>
2025-01-17 21:08:05 -0500

    Enhance Navigation component with clsx for active link styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,35 @@
 import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 
 export default function Navigation() {
-    const navigationItems = [
-        { title : "Home", path: "/" },
-        { title : "Reusability", path: "/reusability" },
-    ]
+  const navigationItems = [
+    { title: "Home", path: "/" },
+    { title: "Reusability", path: "/reusability" },
+  ];
 
-    const navigationElements = navigationItems.map((item) => (
-        <li key={item.title}>
-            <NavLink to={item.path}>{item.title}</NavLink>
-        </li>
-    ));
+  const navigationElements = navigationItems.map((item, index) => (
+    <NavLink
+      key={index}
+      to={item.path}
+      className={({ isActive }) =>
+        clsx(
+          "text-2xl font-medium p-2 rounded-md",
+          {
+            "dark:bg-gray-700": isActive,
+            "opacity-75 hover:opacity-100 transition-all duration-300": !isActive,
+          }
+        )
+      }
+    >
+      {item.title}
+    </NavLink>
+  ));
 
-    return (
-        <nav>
-            {navigationElements}
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="p-2">
+        <div className="flex gap-2">{navigationElements}</div>
+      </div>
+    </nav>
+  );
 }
